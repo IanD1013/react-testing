486f5400b7e5c5481cb826a84efcd594c46fe0e3
IanD1013 <ian.tung1013@gmail.com>
2025-02-05 22:28:45 +1300

    Exercise - testing form feedback

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Toaster } from 'react-hot-toast';
 import ProductForm from '../../src/components/ProductForm';
 import { Category, Product } from '../../src/entities';
 import AllProviders from '../AllProviders';
@@ -106,10 +107,43 @@ describe('ProductForm', () => {
     expectErrorToBeInTheDocument(errorMessage);
   });
 
+  it('should call onSubmit with the correct data', async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+
+    const form = await waitForFormToLoad();
+    await form.fill(form.validData);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars
+    const { id, ...formData } = form.validData;
+
+    expect(onSubmit).toHaveBeenCalledWith(formData);
+  });
+
+  it('should display a toast if submission fails', async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+    onSubmit.mockRejectedValue({});
+
+    const form = await waitForFormToLoad();
+    await form.fill(form.validData);
+
+    const toast = await screen.findByRole('status');
+    expect(toast).toBeInTheDocument();
+    expect(toast).toHaveTextContent(/error/i);
+  });
+
   const renderComponent = (product?: Product) => {
-    render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProviders });
+    const onSubmit = vi.fn();
+
+    render(
+      <>
+        <ProductForm product={product} onSubmit={onSubmit} />
+        <Toaster />
+      </>,
+      { wrapper: AllProviders }
+    );
 
     return {
+      onSubmit,
       expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
         const error = screen.getByRole('alert');
         expect(error).toBeInTheDocument();
@@ -132,7 +166,7 @@ describe('ProductForm', () => {
           id: 1,
           name: 'a',
           price: 1,
-          categoryId: 1,
+          categoryId: category.id,
         };
 
         const fill = async (product: FormData) => {
